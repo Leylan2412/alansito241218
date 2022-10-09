728739a70354e284005f86db5a8b7d489c2fcf1d
Leylan2412 <78246432+Leylan2412@users.noreply.github.com>
2022-10-09 01:11:40 -0500

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,39 @@
+var cacheName = 'v1.0'
+
+var cacheAssets = [
+    'index.html',
+    'acerca.html',
+    'mision.html',
+    'vision.html',
+    'contacto.html',
+    '/js/main.js',
+    '/css/style.css',
+    '/img/sw.jpeg'
+]
+
+//Instalación
+
+self.addEventListener('install', e => {
+    console.log('Service Worker: Instalado');
+    e.waitUntil(
+        caches
+        .open(cacheName)
+        .then(cache => {
+            console.log('Service Worker: Cacheando archivos');
+            cache.addAll(cacheAssets);
+        })
+        .then(() => self.skipWaiting())
+    );
+});
+
+//Activación
+self.addEventListener('activate' , e => {
+    console.log('Service Worker: Instalado');
+});
+
+// Fetch
+
+self.addEventListener('fetch', e => {
+    console.log('Service Worker: Fetching');
+        e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
